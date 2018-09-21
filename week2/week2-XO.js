c6dfbd3ac56b4c32b80f3d5e12b336cff37bec41
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-09-21 13:59:28 +0700

    fix indentasi

--- week2/week2-XO.js
+++ week2/week2-XO.js
@@ -1,15 +1,16 @@
 function xo(str) {
-    var countX = 0;
-    var countO = 0;
-    for (var i = 0; i < str.length; i++) {
-        if (str[i] === 'x') {
-            countX = countX + 1;
-        } else {
-            countO = countO + 1;
-        }
+  var countX = 0;
+  var countO = 0;
+  
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === 'x') {
+        countX = countX + 1;
+    } else {
+        countO = countO + 1;
     }
-    return countX == countO; 
   }
+  return countX == countO; 
+}
   
 console.log(xo('xoxoxo'));
 console.log(xo('oxooxo'));
