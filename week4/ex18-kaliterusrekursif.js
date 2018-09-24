a26237369a684a740cb728aab60bbe83b7df1ab0
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-09-25 06:45:35 +0700

    upload ex18

---
+++ week4/ex18-kaliterusrekursif.js
@@ -0,0 +1,19 @@
+function kaliTerusRekursif(angka) {
+  var angkaStr = angka.toString();
+
+  if (angkaStr.length === 1) {
+    return angka;
+  } else {
+    var result = 1;
+    for (var i = 0; i < angkaStr.length; i++) {
+      result = result * Number(angkaStr[i]);
+    }
+  } return kaliTerusRekursif(result);
+}
+
+// TEST CASES
+console.log(kaliTerusRekursif(66)); // 8
+console.log(kaliTerusRekursif(3)); // 3
+console.log(kaliTerusRekursif(24)); // 8
+console.log(kaliTerusRekursif(654)); // 0
+console.log(kaliTerusRekursif(1231)); // 6
