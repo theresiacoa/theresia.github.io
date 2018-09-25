b9f698c30fa93efd048c4353d50ad5478f498e4c
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-09-25 15:30:20 +0700

    add ex4

---
+++ week5/ex4-graduates.js
@@ -0,0 +1,94 @@
+function graduates (students) {
+  var resultObj = {};
+  //give the skeleton for the obj
+  for (let i = 0; i < students.length; i++) {
+    resultObj[students[i].class] = [];
+  } 
+
+  for (var i = 0; i < students.length; i++) {
+    if (students[i].score >= 75 ) {
+      var obj = {};
+      obj.name = students[i].name;
+      obj.score = students[i].score;
+      resultObj[students[i].class].push(obj);
+    }
+  }
+  return resultObj;
+}
+
+console.log(graduates([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Dimitri', score: 90 }
+//   ],
+//   wolves: [
+//     { name: 'Alexei' , score: 85 },
+//     { name: 'Anastasia', score: 78 }
+//   ]
+// }
+
+console.log(graduates([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+
+console.log(graduates([])); //{}
