8c7c6213a8e0cfd113f795abb77dc48a85de07da
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-09-25 18:01:21 +0700

    add ex12

--- week4/ex12-shoppingTime.js
+++ week4/ex12-shoppingTime.js
@@ -1,8 +1,11 @@
+
 function shoppingTime(memberId, money) {
   var object = {};
 
-  if (memberId === undefined) {
+  if (memberId === '' || memberId === undefined) {
     return 'Mohon maaf, toko X hanya berlaku untuk member saja';
+  } else if (money < 50000) {
+    return '';
   } else {
 
     var cost = [];
@@ -17,11 +20,9 @@ function shoppingTime(memberId, money) {
 
     var barang = [];
     for (var i = 0; i < cost.length; i++) {
-      if (money > cost[i][1]) {
-        money = money - cost[i][1];
+      if (money >= cost[i][1]) {
+        money -= cost[i][1];
         barang.push(cost[i][0]);
-      } else {
-        return "Mohon maaf, uang tidak cukup";
       }
     }
     object.listPurchased = barang;
@@ -31,7 +32,7 @@ function shoppingTime(memberId, money) {
 }
 
 // TEST CASES
-console.log(shoppingTime('1820RzKrnWn08', 24750000));
+console.log(shoppingTime('1820RzKrnWn08', 2475000));
   //{ memberId: '1820RzKrnWn08',
   // money: 2475000,
   // listPurchased:
@@ -41,12 +42,12 @@ console.log(shoppingTime('1820RzKrnWn08', 24750000));
   //    'Sweater Uniklooh',
   //    'Casing Handphone' ],
   // changeMoney: 0 }
-// console.log(shoppingTime('82Ku8Ma742', 170000));
-// //{ memberId: '82Ku8Ma742',
-// // money: 170000,
-// // listPurchased:
-// //  [ 'Casing Handphone' ],
-// // changeMoney: 120000 }
-// console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
-// console.log(shoppingTime('234JdhweRxa53', 15000)); //
-// console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
+console.log(shoppingTime('82Ku8Ma742', 170000));
+//{ memberId: '82Ku8Ma742',
+// money: 170000,
+// listPurchased:
+//  [ 'Casing Handphone' ],
+// changeMoney: 120000 }
+console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
+console.log(shoppingTime('234JdhweRxa53', 15000)); //
+console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
