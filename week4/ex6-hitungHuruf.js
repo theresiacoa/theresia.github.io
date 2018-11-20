431a90845ac35f3dc7746725248f7e1afd028d23
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-11-20 22:53:02 +0700

    add ex6

--- week4/ex6-hitungHuruf.js
+++ week4/ex6-hitungHuruf.js
@@ -1,28 +1,42 @@
-function hitungHuruf(kalimat) {
-  var arrayKata = [];
-
-  for (var i = 0; i < kalimat.length; i++) {
-    var jumlah = 0;
-    var kata = kalimat[i];
-
-    for (var j = 0; j < kata.length; j++) {
-      kata.split("");
-      if (kata[j] === kata[j+1]) {
-        jumlah +=1;
-        arrayKata.push()
-      } else {
-        //kata[j].splice(j,1);
-      }
+function hitungHuruf(kata) {
+  var arr = kata.split(' ');
+  var countMax = 0;
+  var result = '';
+  // var temp = hitungKata(arr);
+  // console.log(arr);
+  for (var i = 0; i < arr.length; i++) {
+    var count = hitungKata(arr[i]);
+    // console.log(temp);
+    if (count > countMax) {
+      countMax = count;
+      result = arr[i];
     }
-
   }
-  return kata;
+  return result;
+}
 
+function hitungKata(arr) {
+  var obj = {};
+  var jumlah = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    if (obj[arr[i]] === undefined) {
+      obj[arr[i]] = 0
+    }
+    obj[arr[i]]++;
+  }
+  
+  for (var i = 0; i < arr.length; i++) {
+    if (obj[arr[i]] > 1) {
+      jumlah++;
+    }
+  }
+  return jumlah;
 }
 
 // TEST CASES
 console.log(hitungHuruf('Today, is the greatest day ever')); // greatest
-// console.log(hitungHuruf('I am a passionate developer')); // passionate
-// console.log(hitungHuruf('aku adalah anak gembala')); // adalah
-// console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
-// console.log(hitungHuruf('mengayuh perahu di danau')); // danau
+console.log(hitungHuruf('I am a passionate developer')); // passionate
+console.log(hitungHuruf('aku adalah anak gembala')); // adalah
+console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
+console.log(hitungHuruf('mengayuh perahu di danau')); // danau
